38c4dbf957fe3281184469dbf0cdc4adf6ff2142
Ayushmaan Pandey <123830487+AyushmaanPandey11@users.noreply.github.com>
2024-11-16 21:16:46 +0530

    Delete db directory

--- db/prisma/migrations/20240810210757_new/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "User" ADD COLUMN     "merchantId" INTEGER;
-
--- AddForeignKey
-ALTER TABLE "User" ADD CONSTRAINT "User_merchantId_fkey" FOREIGN KEY ("merchantId") REFERENCES "Merchant"("id") ON DELETE SET NULL ON UPDATE CASCADE;
